f052dd908b60840dc1eee966f74b5708c965142c
Smidra <rad.smid@gmail.com>
2021-05-14 21:55:55 +0200

    Split into functions

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-function oslovuj() {
-    var name = document.getElementById("inputName").value;
+// Vycisti string od nealfanumerickych znaku a prevede ho na lowercase
+function vycisti(jmeno_vstup) {
     // Vycisti '.' a ',' split do pole
-    nameClean = name.replaceAll(".", ' ');
+    nameClean = jmeno_vstup.replaceAll(".", ' ');
     nameClean = nameClean.replaceAll(',', ' ');
-    nameArray = nameClean.split(/ +/);
+    nameClean = nameClean.replaceAll('	', ' ');
+    nameClean = nameClean.toLowerCase();
+    // Nahradit tituly rozdelene teckou bezteckovou variantou
+    nameClean = nameClean.replaceAll('ph d', 'phd');
+    nameClean = nameClean.replaceAll('th d', 'thd');
+    nameClean = nameClean.replaceAll('ing  arch', 'ingarch');
+    nameClean = nameClean.replaceAll('akad  arch', 'akadarch');
+    nameClean = nameClean.replaceAll('ak  mal', 'akmal');
+    return nameClean
+}
 
-    // Zjisti jestli je titulovany zena
-    var isWoman = false
-    if (name.match(/(ova|ová|á)($|,|\.|\ )/g) !== null) {
-        isWoman = true
+// Zjisti jestli je titulovany zena
+function jeZena(jmeno_vstup) {
+    if (jmeno_vstup.match(/(ova|ová|á)($|,|\.|\ )/g) !== null) {
+        return true
     }
+    return false
+}
 
+// Vymysli spravne osloveni pro vycistene jmeno
+function vymysliOsloveni(nameArray, isWoman) {
     // Pole možných titulů
-    profesorArr = ["prof", "profesor", "Prof"]
+    profesorArr = ["prof", "profesor"]
     docentArr = ["doc", "docnet"]
-    doktorArr = ["Ph", "PhD", "ThD", "CSc", "PhDr", "JUDr", "RNDr", "PharmDr", "ThDr", "MUDr", "MDDr", "MVDr", "PaedDr", "MSDr", "RCDr", "RSDr", "RTDr"]
-    inzenyrArr = ["Ing", "ing"]
-    magistrArr = ["Mgr", "mgr", "MgA", "PhMr"]
-    licencArr = ["ThLic"]
-    bakalarArr = ["bc", "Bc", "BcA"]
+    doktorArr = ["phd", "thd", "csc", "phdr", "judr", "rndr", "pharmdr", "thdr", "mudr", "mddr", "mvdr", "paeddr", "msdr", "rcdr", "rsdr", "rtdr"]
+    inzenyrArr = ["ing", "ingarch"]
+    magistrArr = ["mgr", "mga", "phmr"]
+    licencArr = ["thlic"]
+    bakalarArr = ["bc", "bca"]
 
     // Najdi doszene tituly
     profesor = nameArray.some(r => profesorArr.includes(r))
@@ -32,33 +45,36 @@ function oslovuj() {
     // Vymysli osloveni z hierarchie
     var osloveni = ""
     if (profesor) {
-        if (isWoman) { osloveni = "Vážená paní profesorko," }
-        else { osloveni = "Vážený pane profesore," }
+        if (isWoman) { osloveni = "Vážená paní profesorko," } else { osloveni = "Vážený pane profesore," }
     }
     else if (docent) {
-        if (isWoman) { osloveni = "Vážená paní docentko," }
-        else { osloveni = "Vážený pane docente," }
+        if (isWoman) { osloveni = "Vážená paní docentko," } else { osloveni = "Vážený pane docente," }
     }
     else if (doktor) {
-        if (isWoman) { osloveni = "Vážená paní doktorko," }
-        else { osloveni = "Vážený pane doktore," }
+        if (isWoman) { osloveni = "Vážená paní doktorko," } else { osloveni = "Vážený pane doktore," }
     }
     else if (inzenyr) {
-        if (isWoman) { osloveni = "Vážená paní inženýrko," }
-        else { osloveni = "Vážený pane inženýre," }
+        if (isWoman) { osloveni = "Vážená paní inženýrko," } else { osloveni = "Vážený pane inženýre," }
     }
     else if (magistr) {
-        if (isWoman) { osloveni = "Vážená paní magistro," }
-        else { osloveni = "Vážený pane magistře," }
+        if (isWoman) { osloveni = "Vážená paní magistro," } else { osloveni = "Vážený pane magistře," }
     }
     else if (licenc) {
-        if (isWoman) { osloveni = "Vážená paní licenciátko," }
-        else { osloveni = "Vážený pane licenciáte," }
+        if (isWoman) { osloveni = "Vážená paní licenciátko," } else { osloveni = "Vážený pane licenciáte," }
     }
     else {
-        if (isWoman) { osloveni = "Vážená paní kolegyně," }
-        else { osloveni = "Vážený pane kolego," }
+        if (isWoman) { osloveni = "Vážená paní kolegyně," } else { osloveni = "Vážený pane kolego," }
     }
+    return osloveni
+}
+
+// Provede cely skript. Upravi html s novou hodnotou osloveni.
+function oslovuj() {
+    var name = document.getElementById("inputName").value;
+    var nameClean = vycisti(name)
+    var nameArray = nameClean.split(/ +/);
+    var isWoman = jeZena(nameClean)
+    var osloveni = vymysliOsloveni(nameArray, isWoman)
 
     // Nastav a zobraz
     if (name == "Radek Šmíd") {
@@ -71,16 +87,11 @@ function oslovuj() {
 }
 
 // Poslouchej na tlačítko enter
-// Get the input field
+// Sauce https://stackoverflow.com/questions/155188/trigger-a-button-click-with-javascript-on-the-enter-key-in-a-text-box
 var input = document.getElementById("inputName");
-
-// Execute a function when the user releases a key on the keyboard
 input.addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
+    if (event.key === 'Enter') {
         event.preventDefault();
-        // Trigger the button element with a click
         document.getElementById("oslovit").click();
     }
 });
