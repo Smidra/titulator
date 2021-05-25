c7697388fb04db29801a41f41fbf3c51718e3408
Smidra <rad.smid@gmail.com>
2021-05-25 13:41:16 +0200

    TEST - odendam JS jestli bude klik fungovat

--- test/script.js
+++ test/script.js
@@ -126,26 +126,26 @@ function vymysliOsloveni(nameArray, isWoman) {
 
 // Provede cely skript. Upravi html s novou hodnotou osloveni.
 function oslovuj() {
-    var name = document.getElementById("inputName").value;
-    var nameClean = vycisti(name)
-    var nameArray = nameClean.split(/ +/);
-    var isWoman = jeZena(nameClean)
+    // var name = document.getElementById("inputName").value;
+    // var nameClean = vycisti(name)
+    // var nameArray = nameClean.split(/ +/);
+    // var isWoman = jeZena(nameClean)
 
-    // Zkus oslovit akad. funkci
-    var osloveni_s_funkci = vymysliFunkci(isWoman)
-    var osloveni = osloveni_s_funkci[0]
-    var funkce = osloveni_s_funkci[1]
-    if (osloveni == "") {
-        // Nemel akad fci, oslovujeme tituly
-        osloveni = vymysliOsloveni(nameArray, isWoman)
-        nastavDetail(nameArray, isWoman, false, funkce)
-    } else {
-        // Mel akad fci, neoslovujeme tituly
-        nastavDetail(nameArray, isWoman, true, funkce)
-    }
+    // // Zkus oslovit akad. funkci
+    // var osloveni_s_funkci = vymysliFunkci(isWoman)
+    // var osloveni = osloveni_s_funkci[0]
+    // var funkce = osloveni_s_funkci[1]
+    // if (osloveni == "") {
+    //     // Nemel akad fci, oslovujeme tituly
+    //     osloveni = vymysliOsloveni(nameArray, isWoman)
+    //     nastavDetail(nameArray, isWoman, false, funkce)
+    // } else {
+    //     // Mel akad fci, neoslovujeme tituly
+    //     nastavDetail(nameArray, isWoman, true, funkce)
+    // }
 
     // Nastav a zobraz
-    if (name == "Radek Šmíd") {
+    if ( true ) {
         document.getElementById("dopis").innerHTML = "Husťák";
     }
     else {
