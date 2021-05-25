f947adb2145441be0a0f708b217c4543788df0b4
Smidra <rad.smid@gmail.com>
2021-05-25 15:17:32 +0200

    TESTING - minimal osetreny + clean

--- test/script.js
+++ test/script.js
@@ -129,29 +129,24 @@ function oslovuj() {
     // var name = document.getElementById("inputName").value;
     var name = document.getElementById('inputName') ? document.getElementById('inputName').value : 'bad shit happened';
     var nameClean = vycisti(name)
-    var nameArray = nameClean.split(/ +/);
-    var isWoman = jeZena(nameClean)
+    // var nameArray = nameClean.split(/ +/);
+    // var isWoman = jeZena(nameClean)
 
-    // Zkus oslovit akad. funkci
-    var osloveni_s_funkci = vymysliFunkci(isWoman)
-    var osloveni = osloveni_s_funkci[0]
-    var funkce = osloveni_s_funkci[1]
-    if (osloveni == "") {
-        // Nemel akad fci, oslovujeme tituly
-        osloveni = vymysliOsloveni(nameArray, isWoman)
-        nastavDetail(nameArray, isWoman, false, funkce)
-    } else {
-        // Mel akad fci, neoslovujeme tituly
-        nastavDetail(nameArray, isWoman, true, funkce)
-    }
+    // // Zkus oslovit akad. funkci
+    // var osloveni_s_funkci = vymysliFunkci(isWoman)
+    // var osloveni = osloveni_s_funkci[0]
+    // var funkce = osloveni_s_funkci[1]
+    // if (osloveni == "") {
+    //     // Nemel akad fci, oslovujeme tituly
+    //     osloveni = vymysliOsloveni(nameArray, isWoman)
+    //     nastavDetail(nameArray, isWoman, false, funkce)
+    // } else {
+    //     // Mel akad fci, neoslovujeme tituly
+    //     nastavDetail(nameArray, isWoman, true, funkce)
+    // }
 
     // Nastav a zobraz
-    if (name == "Radek Šmíd") {
-        document.getElementById("dopis").innerHTML = "Husťák";
-    }
-    else {
-        document.getElementById("dopis").innerHTML = osloveni;
-    }
+    document.getElementById("dopis").innerHTML = nameClean;
     document.getElementById("karta").hidden = false
 }
 
